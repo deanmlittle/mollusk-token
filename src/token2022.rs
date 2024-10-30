59aa401d8ad3dc17a18dd73122c3668d925c49e1
Dean 利迪恩 <dean@bitping.com>
2024-10-30 12:29:07 +0800

    Fixed loader key

--- src/token2022.rs
+++ src/token2022.rs
@@ -6,7 +6,7 @@ use solana_sdk::pubkey;
 pub const ID: Pubkey = pubkey!("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb");
 
 pub fn add_program(mollusk: &mut Mollusk) {
-    mollusk.add_program_with_elf_and_loader(&ID, include_bytes!("elf/token_2022.so"), mollusk::program::loader_key::create_program_account_loader_v3);
+    mollusk.add_program_with_elf_and_loader(&ID, include_bytes!("elf/token_2022.so"), &mollusk_svm::program::loader_keys::LOADER_V3);
 }
 
 pub fn account() -> AccountSharedData {
