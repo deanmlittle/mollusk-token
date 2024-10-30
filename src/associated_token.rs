7eef514a497550b45cf07b340f6115f7f04b9b24
Dean 利迪恩 <dean@bitping.com>
2024-10-30 12:30:12 +0800

    Fixed elf path

--- src/associated_token.rs
+++ src/associated_token.rs
@@ -8,7 +8,7 @@ use crate::elf;
 pub const ID: Pubkey = pubkey!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
 
 pub fn add_program(mollusk: &mut Mollusk) {
-    mollusk.add_program_with_elf_and_loader(&ID, include_bytes!("associated_token.so"), &mollusk_svm::program::loader_keys::LOADER_V3);
+    mollusk.add_program_with_elf_and_loader(&ID, include_bytes!("elf/associated_token.so"), &mollusk_svm::program::loader_keys::LOADER_V3);
 }
 
 /// Get the key and account for the system program.
